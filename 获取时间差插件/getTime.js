378cd08a5adfc4e7de5f523ac0109764017b0b9e
白大蒜 <18823547629@163.com>
2018-07-30 16:38:04 +0800

    更正bug
    之前版本，当间隔小于1min返回undefined

--- 获取时间差插件/getTime.js
+++ 获取时间差插件/getTime.js
@@ -1,36 +1,44 @@
 
 function getTime(time){/*接受字符串或对象，返回与当前时间差*/
 	/*动作发生时间*/
-		let theDate;
+	let theDate;
 	if(time instanceof Date){
 		theDate=time;
 	}
 	else{
 		theDate=new Date(time);
 	}
+		console.log(theDate);
 	let	theYear=theDate.getFullYear(),
 		theMonth=theDate.getMonth()+1,
 		theDay=theDate.getDate(),
 		theHour=theDate.getHours(),
 		theMinute=theDate.getMinutes();
-	/*服务器当前时间*/
 	let date = new Date(),
 		nowYear=date.getFullYear(),
 		nowMonth=date.getMonth()+1,
 		nowDay=date.getDate(),
 		nowHour=date.getHours(),
 		nowMinute=date.getMinutes();
-	if(nowYear-theYear>=1)
+	if(nowYear-theYear>=1){
 		return nowYear-theYear+'年前';
-	if(nowMonth-theMonth>=1)
+	}
+	else if(nowMonth-theMonth>=1){
 		return nowMonth-theMonth+'月前';
-	if(nowDay-theDay>=7)
+	}
+	else if(nowDay-theDay>=7){
 		return Math.floor((nowDay-theDay)/7)+'周前';
-	if(nowDay-theDay>=1)
+	}
+	else if(nowDay-theDay>=1){
 		return nowDay-theDay+'天前';
-	if(nowHour-theHour>=1)
+	}
+	else if(nowHour-theHour>=1){
 		return nowHour-theHour+'小时前';
-	if(nowMinute-theMinute>=1)
+	}
+	else if(nowMinute-theMinute>=1){
 		return nowMinute-theMinute+'分钟前';
-}
+	}
+	else{
+		return '刚刚';
+	}
 alert(getTime('2018-7-7 11:11:11'));
