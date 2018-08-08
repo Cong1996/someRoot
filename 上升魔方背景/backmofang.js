6c737b6105e9e8a433738c619d1bb750257e4ab2
白大蒜 <18823547629@163.com>
2018-08-08 16:24:11 +0800

    增加了魔方可视化

--- 上升魔方背景/backmofang.js
+++ 上升魔方背景/backmofang.js
@@ -5,6 +5,8 @@ function initMofang(containerID,maxwidth,minwidth,number,durationMin,durationMax
 	getRandom=(max,min)=>(Math.ceil(Math.random()*(max-min)+min)),
 	vW=window.innerWidth,
 	vH=window.innerHeight;
+	var colorArray=["#339999","#ccccff","#FF4499","#000","#FFCCFF",
+			"rgb(0,108,255)","rgb(0,108,255)","#006633","#9933CC"];
 mofang.prototype={
 	init(){
 		this.ul=document.createElement('ul');
@@ -16,23 +18,39 @@ mofang.prototype={
 		this.duration=getRandom(durationMax,durationMin);
 		this.ul.style.top=this.pointY+'px';
 		this.ul.style.left=this.pointX+'px';
+		this.lineColor=colorArray[getRandom(0,colorArray.length-1)];
 		this.addLi();
 		this.ul.style.animationDuration=this.duration+'s';
 		document.getElementById('wrap').append(this.ul);
 	},
 	addLi(){
-		var style=`width:${this.w}px;height:${this.h}px;`,
-			li="";
-		for(var i=0;i<6;i++){
+		let style=`width:${this.w}px;height:${this.h}px;`,
+			li="",
+			arrayOfMofangColor=['rgb(255,255,0)','rgb(236,142,34)','rgb(0,108,255)','rgb(208,50,0)','rgb(108,255,0)','rgb(255,255,255)'];
+
+		for(let i=0;i<6;i++){
 			switch(i){
-				case 0:style+=`transform:translateZ(${this.w/2}px);`;break;
+				case 0:style+=`transform:translateZ(${this.w/2}px);border-color:${this.lineColor};`;break;
 				case 1:style+=`transform:translateZ(${-this.w/2}px);`;break;
 				case 2:style+=`transform:rotateY(90deg) translateZ(${this.w/2}px);`;break;
 				case 3:style+=`transform:rotateY(90deg) translateZ(${-this.w/2}px);`;break;
 				case 4:style+=`transform:rotateX(90deg) translateZ(${this.w/2}px);`;break;
 				case 5:style+=`transform:rotateX(90deg) translateZ(${-this.w/2}px);`
 			}
-			li+=`<li style="${style}"></li>`
+			li+=`<li style="${style}">
+					<ul style="display:flex;flex-wrap:wrap;box-sizing:border-box;width:100%;height:${this.h}px;background-color:#000;">
+						<li style="flex:1 1 33.3%;width:${this.w/3}px;height:${this.h/3}px;box-sizing:border-box;border:1px solid #000;border-radius:2px;background-color:${arrayOfMofangColor[i]}"></li>
+						<li style="flex:1 1 33.3%;width:${this.w/3}px;height:${this.h/3}px;box-sizing:border-box;border:1px solid #000;border-radius:2px;background-color:${arrayOfMofangColor[i]}"></li>
+						<li style="flex:1 1 33.3%;width:${this.w/3}px;height:${this.h/3}px;box-sizing:border-box;border:1px solid #000;border-radius:2px;background-color:${arrayOfMofangColor[i]}"></li>
+						<li style="flex:1 1 33.3%;width:${this.w/3}px;height:${this.h/3}px;box-sizing:border-box;border:1px solid #000;border-radius:2px;background-color:${arrayOfMofangColor[i]}"></li>
+						<li style="flex:1 1 33.3%;width:${this.w/3}px;height:${this.h/3}px;box-sizing:border-box;border:1px solid #000;border-radius:2px;background-color:${arrayOfMofangColor[i]}"></li>
+						<li style="flex:1 1 33.3%;width:${this.w/3}px;height:${this.h/3}px;box-sizing:border-box;border:1px solid #000;border-radius:2px;background-color:${arrayOfMofangColor[i]}"></li>
+						<li style="flex:1 1 33.3%;width:${this.w/3}px;height:${this.h/3}px;box-sizing:border-box;border:1px solid #000;border-radius:2px;background-color:${arrayOfMofangColor[i]}"></li>
+						<li style="flex:1 1 33.3%;width:${this.w/3}px;height:${this.h/3}px;box-sizing:border-box;border:1px solid #000;border-radius:2px;background-color:${arrayOfMofangColor[i]}"></li>
+						<li style="flex:1 1 33.3%;width:${this.w/3}px;height:${this.h/3}px;box-sizing:border-box;border:1px solid #000;border-radius:2px;background-color:${arrayOfMofangColor[i]}"></li>
+					</ul>
+				</li>`;
+		
 		}
 		this.ul.innerHTML=li;
 	}
@@ -44,4 +62,4 @@ function addMofang(num){
 }
 addMofang(number);
 }
-initMofang('wrap',10,50,20,10,20);
+initMofang('wrap',50,100,40,15,60);
